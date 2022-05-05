a9576f7a249f45d9b9eecca030460e9f1376f1d0
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-05-05 15:23:47 +0200

    Def all functions

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from tkinter import *
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def next_turn():
+    pass
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def check_winner():
+    pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def empty_spaces():
+    pass
+
+
+def new_game():
+    pass
