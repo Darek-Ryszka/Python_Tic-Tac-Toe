3701887901792a98d685320f2474c645cbd9fa1f
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-05-05 15:29:09 +0200

    Add reset button

--- main.py
+++ main.py
@@ -29,4 +29,7 @@ buttons = [[0,0,0],
 label = Label(text=player + " turn", font=('consolas',40))
 label.pack(side="top")
 
+reset_button = Button(text="restart", font=('consolas',20), command=new_game)
+reset_button.pack(side="top")
+
 window.mainloop()
